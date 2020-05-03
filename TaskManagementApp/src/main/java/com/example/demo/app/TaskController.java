0595456e6667a5071e3874c113fe0480b175de2b
松下俊 <shunmatsu753@gmail.com>
2020-05-03 21:58:17 +0900

    セッションが管理されているかの確認

--- TaskManagementApp/src/main/java/com/example/demo/app/TaskController.java
+++ TaskManagementApp/src/main/java/com/example/demo/app/TaskController.java
@@ -6,13 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.example.demo.domain.AccountForm;
-import com.example.demo.domain.AccountSessionData;
+import com.example.demo.domain.Account;
+import com.example.demo.mapper.AccountMapper;
 import com.example.demo.service.TaskService;
 
 @Controller
@@ -21,26 +19,19 @@ import com.example.demo.service.TaskService;
 public class TaskController {
 
 	@Autowired
-	AccountSessionData accountSessionData;
+	AccountMapper accountMapper;
 
 	@Autowired
 	TaskService taskService;
 
-	// お試してService経由無しでセッション管理を行ってみる
 	@GetMapping
-	public String index(@Validated AccountForm accountForm, BindingResult bindingResult, Model model,
-			Principal principal) {
-		accountSessionData.setId(1);
-		accountSessionData.setUsername("hoge");
-		accountSessionData.setEmail("hoge@email.com");
-		accountSessionData.setPassword("password");
+	public String index(Model model, Principal p) {
 
-		model.addAttribute("sesUsername", accountSessionData.getUsername());
-		model.addAttribute("email", accountSessionData.getEmail());
+		String username = p.getName();
+		Account ac = accountMapper.findByUsername(username);
+		int id = ac.getId();
+		model.addAttribute("id", id);
 
-		model.addAttribute("task", taskService.findAll());
-		String name = principal.getName();
-		model.addAttribute("username", name);
 		return "index";
 	}
 }
