d29288bc70d70fa7b2d8a2aa0eead9c79e265239
松下俊 <shunmatsu753@gmail.com>
2020-05-05 08:10:53 +0900

    idとusernameを追加

---
+++ TaskManagementApp/src/main/java/com/example/demo/service/GetLoginUserService.java
@@ -0,0 +1,28 @@
+package com.example.demo.service;
+
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.domain.Account;
+import com.example.demo.mapper.AccountMapper;
+
+@Service
+public class GetLoginUserService {
+
+	@Autowired
+	AccountMapper accountMapper;
+
+	public int getLoginUserId(Principal p) {
+		String username = p.getName();
+		Account ac = accountMapper.findByUsername(username);
+		int userId = ac.getId();
+		return userId;
+	}
+
+	public String getLoginUsername(Principal p) {
+		String name = p.getName();
+		return name;
+	}
+}
