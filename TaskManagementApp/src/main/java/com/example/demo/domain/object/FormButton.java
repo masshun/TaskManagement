fe4092e9ca127fc5bfe746728cb6fa342b546cda
松下俊 <shunmatsu753@gmail.com>
2020-05-27 22:57:44 +0900

    ラジオボタンとセレクトボタンの内容を一元化

--- TaskManagementApp/src/main/java/com/example/demo/domain/object/FormButton.java
+++ TaskManagementApp/src/main/java/com/example/demo/domain/object/FormButton.java
@@ -3,12 +3,14 @@ package com.example.demo.domain.object;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import lombok.Getter;
+import org.springframework.stereotype.Component;
 
-@Getter
+@Component
 public class FormButton {
 
 	private Map<String, String> selectLabel;
+
+	private Map<String, Boolean> statusRadio;
 
 	public Map<String, String> initSelectLabel() {
 		this.selectLabel = new LinkedHashMap<>();
@@ -16,6 +18,13 @@ public class FormButton {
 		selectLabel.put("青 (重要度:中)", "blue");
 		selectLabel.put("緑 (重要度:低)", "green");
 		return this.selectLabel;
+	}
+
+	public Map<String, Boolean> initStatusRadio() {
+		this.statusRadio = new LinkedHashMap<>();
+		statusRadio.put("未完", false);
+		statusRadio.put("完了!", true);
+		return this.statusRadio;
 	}
 
 }
