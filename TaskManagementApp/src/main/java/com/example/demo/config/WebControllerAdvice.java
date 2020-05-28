84d04e32f905532eecc9eebe6ca8349f4edeb7b0
松下俊 <shunmatsu753@gmail.com>
2020-05-28 23:01:45 +0900

    例外ハンドリングの実装 #7

---
+++ TaskManagementApp/src/main/java/com/example/demo/config/WebControllerAdvice.java
@@ -0,0 +1,72 @@
+package com.example.demo.config;
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.mail.MailAuthenticationException;
+import org.springframework.mail.MailParseException;
+import org.springframework.mail.MailPreparationException;
+import org.springframework.mail.MailSendException;
+import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
+import org.springframework.web.HttpSessionRequiredException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.example.demo.exception.MultipleException;
+
+@ControllerAdvice
+@Component
+public class WebControllerAdvice {
+
+	@ExceptionHandler({ MultipleException.class })
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public String handlerException(MultipleException e, Model model) {
+		model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR);
+		model.addAttribute("message", e.getMessage());
+		return "error/systemError";
+	}
+
+	@ExceptionHandler({ DataAccessException.class })
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public String dataAccessException(DataAccessException e, Model model) {
+		model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR);
+		model.addAttribute("message", "データアクセスエラー");
+		return "error/systemError";
+	}
+
+	@ExceptionHandler({ HttpSessionRequiredException.class })
+	public String httpSessionRequiredException(HttpSessionRequiredException e, Model model) {
+		model.addAttribute("status", HttpStatus.BAD_REQUEST);
+		model.addAttribute("message", "セッションエラー");
+		return "error/systemError";
+	}
+
+	@ExceptionHandler({ MailAuthenticationException.class })
+	public String mailException(MailAuthenticationException e, Model model) {
+		model.addAttribute("status", HttpStatus.FORBIDDEN);
+		model.addAttribute("message", "認証に失敗しました");
+		return "error/systemError";
+	}
+
+	@ExceptionHandler({ MailParseException.class })
+	public String mailParceException(MailParseException e, Model model) {
+		model.addAttribute("status", HttpStatus.BAD_REQUEST);
+		model.addAttribute("message", "不正な値を検出しました");
+		return "error/systemError";
+	}
+
+	@ExceptionHandler({ MailPreparationException.class })
+	public String mailPreparationException(MailPreparationException e, Model model) {
+		model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR);
+		model.addAttribute("message", "内部でエラーが発生しました");
+		return "error/systemError";
+	}
+
+	@ExceptionHandler({ MailSendException.class })
+	public String mailSendException(MailSendException e, Model model) {
+		model.addAttribute("status", HttpStatus.HTTP_VERSION_NOT_SUPPORTED);
+		model.addAttribute("実装されていません");
+		return "error/systemError";
+	}
+}
---
+++ TaskManagementApp/src/main/java/com/example/demo/exception/MultipleException.java
@@ -0,0 +1,11 @@
+package com.example.demo.exception;
+
+public class MultipleException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public MultipleException(String msg) {
+		super(msg);
+	}
+
+}
