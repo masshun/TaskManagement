03d8ecd45cc28396de0326ff49e61d675f6eda7c
松下俊 <shunmatsu753@gmail.com>
2020-05-29 22:06:06 +0900

    ログイン及びログアウトのテスト #11

---
+++ TaskManagementApp/src/test/java/com/example/demo/controller/LoginAndLogoutControllerTest.java
@@ -0,0 +1,46 @@
+package com.example.demo.controller;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+@AutoConfigureMockMvc
+public class LoginAndLogoutControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	void ログイン成功後ホーム画面に遷移する() throws Exception {
+		this.mockMvc.perform(formLogin("/login").user("hoge").password("password")).andExpect(status().isFound())
+				.andExpect(redirectedUrl("/"));
+	}
+
+	@Test
+	void DBに存在しないユーザー情報でログインするとログインページへ戻される() throws Exception {
+		this.mockMvc.perform(formLogin("/login").user("unknown").password("unknown"))
+				.andExpect(redirectedUrl("/login"));
+	}
+
+	@Test
+	@WithMockUser(username = "hoge", password = "password")
+	void ログアウト処理でログイン画面に遷移する() throws Exception {
+		this.mockMvc.perform(post("/logout").with(SecurityMockMvcRequestPostProcessors.csrf()))
+				.andExpect(status().is3xxRedirection()).andExpect(redirectedUrl("/login"));
+		this.mockMvc.perform(get("/")).andExpect(redirectedUrl("http://localhost/login"));
+	}
+
+}
